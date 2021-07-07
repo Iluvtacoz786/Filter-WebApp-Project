f7aa76d4abd56343ac4d8841fd30b1c240c398d1
Zoya Rana <74219818+Iluvtacoz786@users.noreply.github.com>
2021-07-07 11:40:53 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){}
+function setup(){
+    canvas=createCanvas(500,400)
+    canvas.center()
+    video=createCapture(VIDEO)
+    vide.hide();
+}
+function draw(){}
+
+function saveIMG(){
+    save("myselfie.png");
+}
